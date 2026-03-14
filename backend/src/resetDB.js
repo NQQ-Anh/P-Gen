5b987f3af2d4d0e6b04099384bc54c7db15b6b8c
NQQ-Anh <ahihigyu255@gmail.com>
2026-03-14 21:29:41 +0700

    hash password

---
+++ backend/src/resetDB.js
@@ -0,0 +1,38 @@
+import mysql from "mysql2/promise";
+
+async function resetDatabase() {
+  try {
+    console.log("⚠ Resetting database...");
+
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "123456",
+      database: "p_gen"
+    });
+
+    // lấy danh sách bảng
+    const [tables] = await connection.query("SHOW TABLES");
+
+    const tableKey = Object.keys(tables[0])[0];
+
+    await connection.query("SET FOREIGN_KEY_CHECKS = 0");
+
+    for (const table of tables) {
+      const tableName = table[tableKey];
+      console.log("Deleting data from:", tableName);
+
+      await connection.query(`TRUNCATE TABLE \`${tableName}\``);
+    }
+
+    await connection.query("SET FOREIGN_KEY_CHECKS = 1");
+
+    await connection.end();
+
+    console.log("✅ Database reset completed!");
+  } catch (error) {
+    console.error("❌ Reset failed:", error);
+  }
+}
+
+resetDatabase();
--- backend/src/setupDatabase.js
+++ backend/src/setupDatabase.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2/promise";
+import bcrypt from "bcrypt";
 
 async function setupDatabase() {
   try {
@@ -137,16 +138,36 @@ async function setupDatabase() {
       ) ENGINE=InnoDB;
       `);
 
-      await connection.query(`
-        INSERT IGNORE INTO Users (id, username, password, email, role) VALUES
-      (1, 'admin1', 'admin111', 'admin1@pgen.vn', 'Admin'),
-      (2, 'admin2', 'admin222', 'admin2@pgen.vn', 'Admin'),
-      (3, 'user1', 'user111', 'user1@pgen.vn', 'User'),
-      (4, 'user2', 'user222', 'user2@pgen.vn', 'User'),
-      (5, 'user3', 'user333', 'user3@pgen.vn', 'User'),
-      (6, 'user4', 'user444', 'user4@pgen.vn', 'User'),
-      (7, 'user5', 'user555', 'user5@pgen.vn', 'User');
-      `);
+      // await connection.query(`
+      //   INSERT IGNORE INTO Users (id, username, password, email, role) VALUES
+      // (1, 'admin1', 'admin111', 'admin1@pgen.vn', 'Admin'),
+      // (2, 'admin2', 'admin222', 'admin2@pgen.vn', 'Admin'),
+      // (3, 'user1', 'user111', 'user1@pgen.vn', 'User'),
+      // (4, 'user2', 'user222', 'user2@pgen.vn', 'User'),
+      // (5, 'user3', 'user333', 'user3@pgen.vn', 'User'),
+      // (6, 'user4', 'user444', 'user4@pgen.vn', 'User'),
+      // (7, 'user5', 'user555', 'user5@pgen.vn', 'User');
+      // `);
+
+
+const users = [
+  { id:1, username:"admin1", password:"admin111", email:"admin1@pgen.vn", role:"Admin"},
+  { id:2, username:"admin2", password:"admin222", email:"admin2@pgen.vn", role:"Admin"},
+  { id:3, username:"user1", password:"user111", email:"user1@pgen.vn", role:"User"},
+  { id:4, username:"user2", password:"user222", email:"user2@pgen.vn", role:"User"},
+  { id:5, username:"user3", password:"user333", email:"user3@pgen.vn", role:"User"},
+  { id:6, username:"user4", password:"user444", email:"user4@pgen.vn", role:"User"},
+  { id:7, username:"user5", password:"user555", email:"user5@pgen.vn", role:"User"}
+];
+
+for (const user of users) {
+  const hash = await bcrypt.hash(user.password,10);
+
+  await connection.query(
+    "INSERT INTO Users (id, username, password, email, role) VALUES (?, ?, ?, ?, ?)",
+    [user.id, user.username, hash, user.email, user.role]
+  );
+}
     console.log("✅ Database & Tables created successfully!");
     await connection.end();
     process.exit();
